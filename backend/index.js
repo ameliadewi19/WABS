74b6d60b594d35ec17ef2adf0f40d1a24bd7d250
ZackyFaishal <zacky.faishal.tif421@polban.ac.id>
2023-11-07 14:39:48 +0700

    add login and CRUD Recipient

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
+const loginRoute = require("./routes/UserRoute.js");
 // const loginRoute = require("./routes/UserRoute.js");
+const recipientRoute = require("./routes/RecipientRoute.js")
 const cors = require('cors');
 
 dotenv.config();
@@ -12,10 +14,11 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
-
+app.use(loginRoute);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(loginRoute);
+app.use(loginRoute);
+app.use(recipientRoute);
 
 app.listen(5005, () => console.log('Server Up and Running...'));
---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { getUser,login, logout} = require("../controllers/LoginController.js");
+const {verifyToken} = require("../middleware/VerifyToken.js");
+const {refreshToken} = require("../controllers/RefreshToken.js");
+
+const router = express.Router(); // Definisikan objek router terlebih dahulu
+
+router.get('/user', getUser);
+router.post('/login', login);
+router.get('/token', refreshToken);
+router.delete('/logout', logout);
+
+
+module.exports = router; // Export objek router
