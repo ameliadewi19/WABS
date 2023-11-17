c098664cb3c6099a681ed1affe1c3eb1252f4ca4
Dafa Nurul Fauziansyah <dafanurulfauziansyah97@gmail.com>
2023-11-17 13:13:06 +0700

    adding activity frontend

--- frontend/src/components/Aktivitas.js
+++ frontend/src/components/Aktivitas.js
@@ -1,98 +1,226 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Modal, Button, Form } from 'react-bootstrap';
 import feather from 'feather-icons';
-import { DataTable } from 'simple-datatables'; 
+import { DataTable } from 'simple-datatables';
 
-const Aktivitas = ({}) => {
-    const location = useLocation();
-    
-    useEffect(() => {
-        feather.replace(); // Replace the icons after component mounts
-        // Initialize the datatable here
-        const table = new DataTable('.datatable');
-    }, []);
+const Activity = ({}) => {
+  const [activityData, setActivityData] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
+    id: '',
+    activity_name: '',
+    activity_date: '',
+    activity_description: '',
+  });
+  const [entriesPerPage, setEntriesPerPage] = useState(10); // Default entries per page
 
-    return (
-        <main id="main" className="main">
-            <div className="pagetitle">
-            <h1>List Aktivitas</h1>
-            <nav>
-                <ol className="breadcrumb">
-                <li className="breadcrumb-item"><a href="/dashboard">Home</a></li>
-                <li className="breadcrumb-item">List Aktivitas</li>
-                </ol>
-            </nav>
-            </div>
+  useEffect(() => {
+    feather.replace(); // Replace the icons after component mounts
+    fetchActivityData();
+  }, []);
 
-            <section className="section">
-            <div className="row">
-                <div className="col-lg-12">
+  useEffect(() => {
+    // Initialize the datatable here
+    if (activityData.length > 0) {
+      const table = new DataTable('.datatable', {
+        columns: [
+          { select: 4, sortable: false }, // Assuming index 4 is the "Actions" column
+        ],
+      });
+    }
+  }, [activityData]);
 
-                <div className="card">
-                    <div className="card-body">
-                    <h5 className="card-title">List Aktivitas</h5>
-                    <p>Add lightweight datatables to your project with using the <a href="https://github.com/fiduswriter/Simple-DataTables" target="_blank">Simple DataTables</a> library. Just add <code>.datatable</code> className name to any table you wish to conver to a datatable</p>
+  const fetchActivityData = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5005/activity?limit=${entriesPerPage}`);
+      setActivityData(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
-                    <table className="table datatable">
-                        <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Position</th>
-                            <th scope="col">Age</th>
-                            <th scope="col">Start Date</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Brandon Jacob</td>
-                            <td>Designer</td>
-                            <td>28</td>
-                            <td>2016-05-25</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Bridie Kessler</td>
-                            <td>Developer</td>
-                            <td>35</td>
-                            <td>2014-12-05</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">3</th>
-                            <td>Ashleigh Langosh</td>
-                            <td>Finance</td>
-                            <td>45</td>
-                            <td>2011-08-12</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">4</th>
-                            <td>Angus Grady</td>
-                            <td>HR</td>
-                            <td>34</td>
-                            <td>2012-06-11</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">5</th>
-                            <td>Raheem Lehner</td>
-                            <td>Dynamic Division Officer</td>
-                            <td>47</td>
-                            <td>2011-04-19</td>
-                        </tr>
-                        </tbody>
-                    </table>
+  const deleteActivity = async (id) => {
+    console.log('ID to be deleted:', id);
+    try {
+      await axios.delete(`http://localhost:5005/activity/${id}`);
+      fetchActivityData();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  const confirmDelete = (id) => {
+    const shouldDelete = window.confirm('Are you sure you want to delete this data?');
+    if (shouldDelete) {
+      deleteActivity(id);
+    }
+  };
+
+  const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setFormData({
+      id: '',
+      activity_name: '',
+      activity_date: '',
+      activity_description: '',
+    });
+  };
+
+  const handleEdit = (activity) => {
+    setFormData({
+      id: activity.id,
+      activity_name: activity.activity_name,
+      activity_date: activity.activity_date,
+      activity_description: activity.activity_description,
+    });
+    setShowModal(true);
+  };
+
+  const handleSubmit = async () => {
+    if (formData.id) {
+      try {
+        await axios.put(`http://localhost:5005/activity/${formData.id}`, formData, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        fetchActivityData();
+        handleCloseModal();
+      } catch (error) {
+        console.error('Error updating data:', error);
+      }
+    } else {
+      try {
+        await axios.post('http://localhost:5005/activity', formData, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+        fetchActivityData();
+        handleCloseModal();
+      } catch (error) {
+        console.error('Error saving data:', error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleEntriesPerPageChange = (e) => {
+    const selectedEntriesPerPage = parseInt(e.target.value, 10);
+    setEntriesPerPage(selectedEntriesPerPage);
+    fetchActivityData();
+  };
+
+  const handleTambahAktivitasClick = () => {
+    console.log('Tambah Aktivitas clicked!');
+  };
+
+  return (
+    <main id="main" className="main">
+      {/* ... Other code ... */}
+      <section className="section">
+        <div className="row">
+          <div className="card mt-5">
+            <div className="card-body">
+              <h5 className="card-title">Activity</h5>
+              <p>Here is the list of activities.</p>
+
+              <div className="d-flex mb-3">
+                <div className="mr-2">
+                  <label>Show entries:</label>
+                  <select value={entriesPerPage} onChange={handleEntriesPerPageChange}>
+                    <option value="10">10</option>
+                    <option value="25">25</option>
+                    <option value="50">50</option>
+                  </select>
+                </div>
+              </div>
+              <div className='row'>
+                    <div className='col-md-4'>
+                    <button onClick={handleTambahAktivitasClick} className="btn btn-primary">
+                        Tambah Aktivitas
+                    </button>
                     </div>
                 </div>
-
-                </div>
+              <table className="table datatable">
+                <thead>
+                  <tr>
+                    <th>No</th>
+                    <th>Activity Name</th>
+                    <th>Activity Date</th>
+                    <th>Activity Description</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {activityData.map((activity, index) => (
+                    <tr key={activity.id}>
+                      <td>{index + 1}</td>
+                      <td>{activity.activity_name}</td>
+                      <td>{activity.activity_date}</td>
+                      <td>{activity.activity_description}</td>
+                      <td>
+                        <button
+                          onClick={() => handleEdit(activity)}
+                          className="btn btn-primary mt-2 border"
+                          style={{ marginRight: '5px' }}
+                        >
+                          Edit
+                        </button>
+                        <button onClick={() => confirmDelete(activity.id)} className="btn btn-danger mt-2 border">
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-            </section>
-
-        </main>
-
-    );
+          </div>
+        </div>
+      </section>
+      {/* Render the modal if showModal is true */}
+      {showModal && (
+        <Modal show={showModal} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>{formData.id ? `Edit Activity` : 'Add Activity'}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              {/* ... Other form groups ... */}
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleCloseModal}>
+              Cancel
+            </Button>
+            {formData.id ? (
+              <button onClick={() => handleSubmit(formData)} className="btn btn-primary">
+                Edit
+              </button>
+            ) : (
+              <button onClick={handleSubmit} className="btn btn-primary">
+                Add
+              </button>
+            )}
+          </Modal.Footer>
+        </Modal>
+      )}
+    </main>
+  );
 };
 
-export default Aktivitas;
+export default Activity;
