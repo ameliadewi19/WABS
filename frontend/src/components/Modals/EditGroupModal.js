4a6318d24592d4a41df28ed744bae9fe3dbf3e9c
ameliadewi19 <ameliadewi1929@gmail.com>
2023-11-17 12:07:51 +0700

    add group and edit

---
+++ frontend/src/components/Modals/EditGroupModal.js
@@ -0,0 +1,173 @@
+import React, { useState, useRef, useEffect } from 'react';
+import axios from 'axios';
+
+const EditGroupModal = ({ reloadData, groupId }) => {
+  const [groupData, setGroupData] = useState({
+    nama_grup: '',
+    deskripsi: '',
+    recipients: [],
+  });
+  const [recipientOptions, setRecipientOptions] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const modalRef = useRef();
+
+  useEffect(() => {
+    console.log("Inside useEffect - showModal:", showModal, "groupId:", groupId);
+    const fetchRecipientOptions = async () => {
+      try {
+        const response = await axios.get('http://localhost:5005/recipient');
+        setRecipientOptions(response.data);
+        console.log("Recipient options:", response.data);
+      } catch (error) {
+        console.error('Error fetching recipient data:', error);
+      }
+    };
+  
+    fetchRecipientOptions();
+  
+    // Load existing group details when the modal is shown
+    if (groupId) {
+      console.log("Loading group details...");
+      loadGroupDetails(groupId);
+    }
+  }, [showModal, groupId]);  
+
+  const loadGroupDetails = async (groupId) => {
+    try {
+      const response = await axios.get(`http://localhost:5005/groups/${groupId}`);
+      setGroupData(response.data);
+      console.log("Response data:", response.data);
+    } catch (error) {
+      console.error('Error fetching group details:', error);
+    }
+  };  
+
+  const handleGroupChange = (e) => {
+    setGroupData((prevData) => ({
+      ...prevData,
+      nama_grup: e.target.value,
+    }));
+  };
+
+  const handleDeskripsiChange = (e) => {
+    setGroupData((prevData) => ({
+      ...prevData,
+      deskripsi: e.target.value,
+    }));
+  };
+
+  const handleRecipientChange = (e, index) => {
+    const { name, value } = e.target;
+    const newRecipients = [...groupData.recipients];
+    newRecipients[index][name] = value === "" ? null : value;
+    setGroupData((prevData) => ({
+      ...prevData,
+      recipients: newRecipients,
+    }));
+  };  
+
+  const addRecipient = () => {
+    setGroupData((prevData) => ({
+      ...prevData,
+      recipients: [...prevData.recipients, { id_recipient: null }],
+    }));
+  };
+
+  const removeRecipient = (index) => {
+    const newRecipients = [...groupData.recipients];
+    newRecipients.splice(index, 1);
+    setGroupData((prevData) => ({
+      ...prevData,
+      recipients: newRecipients,
+    }));
+  };
+
+  const handleSave = async () => {
+    try {
+      const filteredRecipients = groupData.recipients.filter(recipient => recipient.id_recipient != null);
+      await axios.put(`http://localhost:5005/groups/${groupId}`, {
+        nama_grup: groupData.nama_grup,
+        deskripsi: groupData.deskripsi,
+        recipients: filteredRecipients,
+      });
+
+      reloadData();
+      modalRef.current.click();
+    } catch (error) {
+      console.error('Error updating group:', error);
+    }
+  }; 
+
+  return (
+    <div className={`modal fade`} id="editGroupModal" tabIndex="-1" aria-labelledby="editGroupModalLabel" aria-hidden={!showModal}>
+      <div className="modal-dialog modal-dialog-centered modal-lg">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title" id="editGroupModalLabel">Edit Group Recipient</h5>
+            <button type="button" className="d-none" ref={modalRef} data-bs-dismiss="modal"></button>
+          </div>
+          <div className="modal-body">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="namagroup" className="form-label">Nama Grup</label>
+                <input
+                  className="form-control"
+                  type="text"
+                  placeholder="Masukkan nama grup"
+                  value={groupData.nama_grup}
+                  onChange={handleGroupChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="pembuka" className="form-label">Deskripsi</label>
+                <textarea
+                  className="form-control"
+                  type="text"
+                  placeholder="Masukkan deskripsi grup"
+                  style={{ height: '100px' }}
+                  value={groupData.deskripsi}
+                  onChange={handleDeskripsiChange}
+                ></textarea>
+              </div>
+              {groupData.recipients.map((recipient, index) => (
+                <div key={index} className="mb-3">
+                  <label htmlFor={`recipient-${index}`} className="form-label">Recipient {index + 1}</label>
+                  <div className="input-group">
+                    <select
+                      className="form-select"
+                      name="id_recipient"
+                      value={recipient.id_recipient}
+                      onChange={(e) => handleRecipientChange(e, index)}
+                      required
+                    >
+                      <option value="" disabled selected>
+                        Pilih kontak
+                      </option>
+                      {recipientOptions.map((option) => (
+                        <option key={option.id} value={option.id}>
+                          {option.nama} ({option.no_whatsapp})
+                        </option>
+                      ))}
+                    </select>
+                    <button type="button" className="btn btn-danger" onClick={() => removeRecipient(index)}>
+                      <i className='bi bi-trash'></i>
+                    </button>
+                  </div>
+                </div>
+              ))}
+              <button type="button" className="btn btn-primary" onClick={addRecipient}>
+                <i className='bi bi-plus'></i> Add Recipient
+              </button>
+            </form>
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => setShowModal(false)}>Tutup</button>
+            <button type="button" className="btn btn-primary" onClick={handleSave}>Simpan</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditGroupModal;
